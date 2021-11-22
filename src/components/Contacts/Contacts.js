07a9b7bd1571a94f46badadc15a84b97da9745ee
JoROSU <jovana.romcevicsukalo@gmail.com>
2021-11-22 18:36:30 +0100

    Form modification
     - hidden input field

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -170,8 +170,8 @@ function Contacts() {
 
                     <div className='contacts-form'>
                     
-                        <form name="contact" method="post" netlify netlify-honeypot="bot-field">
-                        
+                        <form name="contact" method="post" netlify netlify-honeypot="bot-field"  hidden>
+                            <input type="hidden" name="form-name" value="contact" />
                             <div className='input-container'>
                                 <label htmlFor='Name' className={classes.label}>
                                     Name
